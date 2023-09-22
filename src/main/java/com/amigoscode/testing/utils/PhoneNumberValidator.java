7e26825262a81af837b137badc32b5508c6ab5ff
Saruu1505 <saruushan1998@hotmail.com>
2023-09-22 19:11:25 +0200

    Update PhoneNumberValidator.java

--- src/main/java/com/amigoscode/testing/utils/PhoneNumberValidator.java
+++ src/main/java/com/amigoscode/testing/utils/PhoneNumberValidator.java
@@ -3,8 +3,7 @@ package com.amigoscode.testing.utils;
 import java.util.function.Predicate;
 
 public class PhoneNumberValidator implements Predicate <String> {
-     {
-    }
+
 
     @Override
     public boolean test(String phoneNumber) {
